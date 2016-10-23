1c4999129c966c1f76f9a7ada9406b7eb74cd7df
Academy Github Workshop <academy.github.workshop@gmail.com>
2016-10-23 18:58:54 +0200

    Add the first version of the service
    Add the first version of the back-end part of the service.
    Essentially just a generated Spring Boot project with
    a simple index page.

---
+++ src/main/java/edu/workshop/api/indexPage.java
@@ -0,0 +1,15 @@
+package edu.workshop.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class indexPage {
+
+  @GetMapping("/")
+  public ModelAndView showResult() {
+    return new ModelAndView("index");
+  }
+
+}
