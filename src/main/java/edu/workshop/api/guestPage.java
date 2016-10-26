3d83a35c2a9b2358e7df838af48573c2aa5c0b65
Academy Github Workshop <academy.github.workshop@gmail.com>
2016-10-26 11:12:40 +0200

    Add @Controller annotation to guestPage controller

--- src/main/java/edu/workshop/api/guestPage.java
+++ src/main/java/edu/workshop/api/guestPage.java
@@ -1,8 +1,10 @@
 package edu.workshop.api;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class guestPage {
 
     @GetMapping("/guest")
